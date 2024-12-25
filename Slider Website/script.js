cfb4a4f3680a84cd1561a3e71f3dec6dc3386eb7
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2024-12-25 11:27:47 +0700

    added UPDATE 7: click thumbnail to show feature, and hover animation on thumbnail

--- Slider Website/script.js
+++ Slider Website/script.js
@@ -5,9 +5,9 @@ const next = document.querySelector(`#next`);
 const prev = document.querySelector(`#prev`);
 
 // Autoplay Slider
-let runAutoPlay = setInterval(() => {
-  next.click();
-}, 5000);
+// let runAutoPlay = setInterval(() => {
+//   next.click();
+// }, 5000);
 
 // let runAutoPlay = setTimeout(() => {
 //   next.click();
@@ -24,19 +24,19 @@ prev.addEventListener(`click`, () => {
 const initSlider = (type) => {
   const sliderItems = listBackgroundImage.querySelectorAll(`.item`);
   const thumbnailItem = thumbnail.querySelectorAll(`.item`);
-  if (type === 'next') {
+  if (type === "next") {
     listBackgroundImage.appendChild(sliderItems[0]);
     thumbnail.appendChild(thumbnailItem[0]);
-    slider.classList.add('next');
+    slider.classList.add("next");
   } else {
     const lastItemOrder = sliderItems.length - 1;
     listBackgroundImage.prepend(sliderItems[lastItemOrder]);
     thumbnail.prepend(thumbnailItem[lastItemOrder]);
-    slider.classList.add('prev');
+    slider.classList.add("prev");
   }
   setTimeout(() => {
-    slider.classList.remove('next');
-    slider.classList.remove('prev');
+    slider.classList.remove("next");
+    slider.classList.remove("prev");
   }, 2000);
 
   // clearTimeout(runAutoPlay);
@@ -47,9 +47,28 @@ const initSlider = (type) => {
 };
 
 // Cick thumbnail to enlarge image
-// const selectThumbnailItem = thumbnail.querySelectorAll(`.item`);
-// const selectedThumbnailItem = selectThumbnailItem.forEach((item) => {
-//   item.addEventListener('click', () => {
+const selectThumbnailItems = thumbnail.querySelectorAll(`.item`);
+const selectSliderItems = listBackgroundImage.querySelectorAll(`.item`);
+console.log(selectThumbnailItems);
+const selectedThumbnailItem = selectThumbnailItems.forEach((item) => {
+  item.addEventListener("click", (event) => {
+    // Manipulate thumbnail image
+    console.log(event.target);
+    thumbnail.prepend(item);
+    thumbnailClassName = item.classList.item(1);
+    console.log(thumbnailClassName);
 
-//   });
-// });
+    // Manipulate slider Image
+    selectSliderItems.forEach((item) => {
+      if (thumbnailClassName === item.classList.item(1))
+        listBackgroundImage.prepend(item);
+    });
+    slider.classList.add("next");
+    setTimeout(() => {
+      slider.classList.remove("next");
+      slider.classList.remove("prev");
+    }, 2000);
+    event.preventDefault();
+    event.stopPropagation();
+  });
+});
