4a616f99de5d32815ff3e44afb4c9ece297d7355
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2024-12-26 06:12:49 +0700

    Simple Counter Done

--- Passenger Counter App/notes/index.js
+++ Passenger Counter App/notes/index.js
@@ -5,6 +5,15 @@ const welcomeEl = document.getElementById("welcome-el");
 // and the greeting we want to render on the page
 let name = "Andi Jafar";
 let greeting = "Welcome Back, ";
+let message = name + greeting + " ";
 
 // Render the welcome message using welcomeEl.innerText
 welcomeEl.innerText = name + greeting;
+
+// Add an emoji to the end! 👋
+// WRITE YOUR CODE BELOW HERE
+// HINT: count = count + 1
+
+welcomeEl.innerText = message + "👋";
+// Or
+welcomeEl.innerHTML += "👋";
--- Passenger Counter App/script.js
+++ Passenger Counter App/script.js
@@ -10,15 +10,30 @@
 // change the count-el in the HTML to reflect the new count
 // Camel Case
 const countElement = document.getElementById("count-el"); // Pass in argument
+const saveEl = document.getElementById("save-el");
 console.log(countElement);
 let count = 0;
 function incrementCount() {
   count++;
+  //   count += 1;
   console.log(count);
   countElement.innerText = count;
 }
 
 // 1. Create a function, save(), which logs out the count when it's called
 function save() {
+  // 1. Grab the save-el paragrah and store it in a variable called saveEl
+  // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+  let savelog = ` ${count} - `;
+  // 3. Render the variable in the saveEl using innerText
+  saveEl.textContent += savelog;
+  count = 0;
+  countElement.textContent = 0;
+  // NB: Make sure to not delete the existing content of the paragraph
   console.log(count);
+}
+
+function reset() {
+  count = 0;
+  countElement.innerText = count;
 }
