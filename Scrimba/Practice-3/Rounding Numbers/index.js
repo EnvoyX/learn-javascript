4d543c260829917586ff92f08151386d44823079
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2024-12-28 06:29:40 +0700

    Practice 3 Done

--- Scrimba/Practice-3/Rounding Numbers/index.js
+++ Scrimba/Practice-3/Rounding Numbers/index.js
@@ -2,7 +2,13 @@
 // Round the price in the button down to two decimal places.
 // Don't know which method to use? Google it!
 
-const totalPrice = 420.69235632455
-const btn = document.getElementById("purchase-btn")
-btn.textContent = `Buy €${ totalPrice }`
+const totalPrice = 420.69235632455;
+const btn = document.getElementById("purchase-btn");
+btn.textContent = `Buy €${totalPrice.toFixed(2)}`;
 
+// Challenge:
+// The toFixed() method doesn't work anymore. Can you make it work?
+// Google the error message if you're unsure about how to do it
+const price2 = "320.3425325";
+const btn2 = document.getElementById("purchase-btn-2");
+btn2.textContent = `Buy €${parseFloat(price2).toFixed(2)}`;
