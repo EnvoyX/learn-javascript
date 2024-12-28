b3c79c8d10bbbc560672aae824967af24d778c69
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2024-12-28 16:02:55 +0700

    Update Async & Await.js

--- Javascript ES6+/Async & Await.js
+++ Javascript ES6+/Async & Await.js
@@ -57,8 +57,9 @@ const apiUrl = "https://api.chucknorris.io/jokes/random";
 
 async function getJoke() {
   const response = await fetch(apiUrl);
-  const data = await response.json();
+  console.log(response);
 
+  const data = await response.json();
   console.log(data);
   console.log(data.value);
 }
